64158049fc26a955f12331e81c235814a79fb1f9
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2025-08-19 10:09:36 +0000

    style: format code for better readability in fantasticonrc.js and svgo.config.js

--- svgo.config.js
+++ svgo.config.js
@@ -1,9 +1,16 @@
+// svgo.config.js
 module.exports = {
   multipass: true,
   plugins: [
-    { name: "preset-default", params: { overrides: { removeViewBox: false } } },
+    {
+      name: "preset-default",
+      params: {
+        overrides: {},
+      },
+    },
+    "removeViewBox", // <-- on l’ajoute séparément
     { name: "removeDimensions", active: true },
     { name: "removeAttrs", params: { attrs: "(fill|stroke)" } },
-    { name: "convertPathData", active: true }
-  ]
+    { name: "convertPathData", active: true },
+  ],
 };
